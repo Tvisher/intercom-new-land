dd99b8a493c7d8913814266c7ce53b5236792164
Ivan Tvisher <Tvisher@mail.ru>
2025-07-28 12:51:24 +0500

    add some comntent

--- js/app.js
+++ js/app.js
@@ -7,3 +7,26 @@ const secondSectionSlider = new Swiper('.second-section-slider', {
         prevEl: '.second-section .swiper-button-prev',
     },
 });
+
+
+const skeakersSlider = new Swiper('.six-section-slider', {
+    slidesPerView: 'auto',
+    speed: 1000,
+    spaceBetween: 33,
+    navigation: {
+        nextEl: '.six-section .swiper-button-next',
+        prevEl: '.six-section .swiper-button-prev',
+    },
+});
+
+
+const videoElParent = document.querySelector('.seven-section__video-wrapper');
+const videoEl = videoElParent.querySelector('video');
+
+videoEl.addEventListener('play', () => {
+    videoElParent.classList.add('video-playing');
+});
+
+videoEl.addEventListener('pause', () => {
+    videoElParent.classList.remove('video-playing');
+});
