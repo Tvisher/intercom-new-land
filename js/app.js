a57994437b5441a0e420e23b02e789a085e6c375
Ivan Tvisher <Tvisher@mail.ru>
2025-08-27 17:00:03 +0500

    add some fix

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ const secondSectionSlider = new Swiper('.second-section-slider', {
     spaceBetween: 20,
     centeredSlides: isMobile,
     centeredSlidesBounds: isMobile,
+    pagination: {
+        el: '.second-section-slider .swiper-pagination',
+        type: 'bullets',
+        clickable: 1,
+    },
     breakpoints: {
         680: {
             spaceBetween: 30
@@ -24,6 +29,11 @@ const skeakersSlider = new Swiper('.six-section-slider', {
     speed: 1000,
     centeredSlides: isMobile,
     centeredSlidesBounds: isMobile,
+    pagination: {
+        el: '.six-section-slider .swiper-pagination',
+        type: 'bullets',
+        clickable: 1,
+    },
     spaceBetween: 20,
     breakpoints: {
         680: {
@@ -50,9 +60,14 @@ const partnersSlider = new Swiper('.ten-section-slider', {
     },
     centeredSlides: isMobile,
     centeredSlidesBounds: isMobile,
-    autoplay: {
-        delay: 5000,
+    pagination: {
+        el: '.ten-section-slider .swiper-pagination',
+        type: 'bullets',
+        clickable: 1,
     },
+    // autoplay: {
+    //     delay: 5000,
+    // },
 });
 
 
@@ -112,3 +127,42 @@ function pad(num) {
 
 updateTimer(); // запускаем сразу
 const intervalId = setInterval(updateTimer, 1000);
+
+
+
+const mobMenu = document.querySelector('.mob-menu');
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.closest('.mob-menu-btn')) {
+        mobMenu.classList.add('open');
+    }
+
+    if (target.closest('.mob-menu-btn-close')) {
+        mobMenu.classList.remove('open');
+    }
+
+    if (target.closest('.mob-menu a')) {
+        mobMenu.classList.remove('open');
+    }
+})
+
+
+
+
+AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 600, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
